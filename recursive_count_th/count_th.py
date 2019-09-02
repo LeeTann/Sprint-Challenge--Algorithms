e0405ff149e91498ee98c10dc0ca1fd39918be62
ltann <leeroyleeroy18@gmail.com>
2019-09-01 20:02:55 -0700

    finsihed count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,26 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+  count = 0
+  substring = "th"
+
+  # find the substring in word
+  substring_index = word.find(substring) 
+
+  if substring_index >= 0:
+
+    #increment count
+    cur_count = count + 1
+    print(cur_count)
+
+    # slice off word from where substring is found
+    sliced_word = word[substring_index + len(substring):]
+    print("word-slice", sliced_word)
+
+    # increment count and recursively call count_th
+    count = cur_count + count_th(sliced_word)
+  
+  return count
+
+print(count_th("thethetoothhurts"))
